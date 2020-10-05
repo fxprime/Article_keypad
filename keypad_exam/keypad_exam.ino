005ed06a92a13b020a4757f53817d6ad667212b6
tdnet <paheyisoicus@gmail.com>
2020-10-05 20:54:48 +0700

    first commit

---
+++ keypad_exam/keypad_exam.ino
@@ -0,0 +1,36 @@
+
+/****************************
+ * www.ModuleMore.com
+ * ตัวอย่างโปรแกรมอ่านค่าจาก Keypad
+ * Wiring
+ * Pin จากซ้ายไปขวา
+ * 9, 8, 7, 6, 5, 4, 3, 2
+ ****************************/
+#include<Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+  char customKey = customKeypad.getKey();
+
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
